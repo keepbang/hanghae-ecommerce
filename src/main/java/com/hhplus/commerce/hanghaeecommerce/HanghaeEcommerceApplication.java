0f9f8c5dfbcae269fb0b008f8ce46329d34ec404
keepbang <rlqud1125@gmail.com>
2024-04-01 10:53:38 +0900

    init project

---
+++ src/main/java/com/hhplus/commerce/hanghaeecommerce/HanghaeEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.commerce.hanghaeecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeEcommerceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HanghaeEcommerceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hhplus/commerce/hanghaeecommerce/HanghaeEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.commerce.hanghaeecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeEcommerceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
