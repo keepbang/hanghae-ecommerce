26583bc87fd27580f8783a9b57d547686aadaae2
keepbang@kakao.com <rlqud1125@naver.com>
2024-04-19 11:47:17 +0900

    feat: 통합테스트 공통 기능 분리

---
+++ src/test/java/com/hhplus/commerce/app/init/IntegrationTest.java
@@ -0,0 +1,72 @@
+package com.hhplus.commerce.app.init;
+
+import com.hhplus.commerce.app.product.dto.ProductRequest;
+import com.hhplus.commerce.app.product.repository.InventoryJpaRepository;
+import com.hhplus.commerce.app.product.repository.ProductJpaRepository;
+import com.hhplus.commerce.app.product.service.ProductService;
+import com.hhplus.commerce.app.user.domain.User;
+import com.hhplus.commerce.app.user.dto.ChargeRequest;
+import com.hhplus.commerce.app.user.repository.UserJpaRepository;
+import com.hhplus.commerce.app.user.repository.WalletJpaRepository;
+import com.hhplus.commerce.app.user.service.WalletService;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * create on 4/19/24. create by IntelliJ IDEA.
+ *
+ * <p> 클래스 설명 </p>
+ *
+ * @author Gibyung Chae (Keepbang)
+ * @version 1.0
+ * @since 1.0
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IntegrationTest {
+
+  // 상품
+  @Autowired
+  InventoryJpaRepository inventoryJpaRepository;
+  @Autowired
+  ProductJpaRepository productJpaRepository;
+  @Autowired
+  ProductService productService;
+
+  // 사용자
+  @Autowired
+  UserJpaRepository userJpaRepository;
+  @Autowired
+  WalletJpaRepository walletJpaRepository;
+  @Autowired
+  WalletService walletService;
+
+  private UUID userKey = UUID.randomUUID();
+
+  @BeforeEach
+  void setUp() {
+    inventoryJpaRepository.deleteAll();
+    productJpaRepository.deleteAll();
+    userJpaRepository.deleteAll();
+    walletJpaRepository.deleteAll();
+
+    productService.save(new ProductRequest("장난감", 1L, 10));
+    productService.save(new ProductRequest("바지", 1L, 10));
+    productService.save(new ProductRequest("티셔츠", 1L, 10));
+    productService.save(new ProductRequest("머그컵", 1L, 10));
+    productService.save(new ProductRequest("마우스", 1L, 10));
+    productService.save(new ProductRequest("키보드", 1L, 10));
+
+    userJpaRepository.save(new User(userKey, "kim", "서울 강남구"));
+    walletService.charge(new ChargeRequest(
+        userKey, 10000L
+    ));
+  }
+
+  public UUID getUserKey() {
+    return userKey;
+  }
+
+
+}
--- src/test/java/com/hhplus/commerce/app/order/OrderIntegrationTest.java
+++ src/test/java/com/hhplus/commerce/app/order/OrderIntegrationTest.java
@@ -4,25 +4,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.hhplus.commerce.app.common.type.PaymentType;
 import com.hhplus.commerce.app.common.type.RecommendType;
+import com.hhplus.commerce.app.init.IntegrationTest;
 import com.hhplus.commerce.app.order.dto.OrderItemRequest;
 import com.hhplus.commerce.app.order.dto.OrderRequest;
 import com.hhplus.commerce.app.order.dto.RecommendProductResponse;
+import com.hhplus.commerce.app.order.repository.OrderItemJpaRepository;
+import com.hhplus.commerce.app.order.repository.OrderJpaRepository;
 import com.hhplus.commerce.app.order.service.OrderService;
-import com.hhplus.commerce.app.product.dto.ProductRequest;
-import com.hhplus.commerce.app.product.service.ProductService;
-import com.hhplus.commerce.app.user.domain.User;
-import com.hhplus.commerce.app.user.dto.ChargeRequest;
-import com.hhplus.commerce.app.user.repository.UserJpaRepository;
-import com.hhplus.commerce.app.user.service.WalletService;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * create on 4/16/24. create by IntelliJ IDEA.
@@ -33,47 +27,30 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
  * @version 1.0
  * @since 1.0
  */
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ExtendWith(SpringExtension.class)
-public class OrderIntegrationTest {
+public class OrderIntegrationTest extends IntegrationTest {
 
-  // 상품
   @Autowired
-  private ProductService productService;
-
-  // 사용자
+  private OrderJpaRepository orderJpaRepository;
   @Autowired
-  private UserJpaRepository userJpaRepository;
-  @Autowired
-  private WalletService walletService;
+  private OrderItemJpaRepository orderItemJpaRepository;
 
   // 주문
   @Autowired
   private OrderService orderService;
 
+  @BeforeEach
+  void setUp() throws Exception {
+    orderJpaRepository.deleteAll();
+    orderItemJpaRepository.deleteAll();
+  }
+
   @DisplayName("최근 3일동안 가장 많이 주문한 상품 5개 조회")
   @Test
   void getRecommendProduct_Recommend1() throws Exception {
     // given
-    // 상품/재고 등록
-    productService.save(new ProductRequest("장난감", 1L, 10));
-    productService.save(new ProductRequest("바지", 1L, 10));
-    productService.save(new ProductRequest("티셔츠", 1L, 10));
-    productService.save(new ProductRequest("머그컵", 1L, 10));
-    productService.save(new ProductRequest("마우스", 1L, 10));
-    productService.save(new ProductRequest("키보드", 1L, 10));
-
-    // 사용자 등록, 지갑 충전
-    UUID userKey = UUID.randomUUID();
-
-    userJpaRepository.save(new User(userKey, "kim", "서울 강남구"));
-    walletService.charge(new ChargeRequest(
-        userKey, 10000L
-    ));
-
     // 주문
     orderService.order(new OrderRequest(
-        userKey,
+        this.getUserKey(),
         PaymentType.WALLET,
         "서울 강남구",
         LocalDateTime.now(),
@@ -85,7 +62,7 @@ public class OrderIntegrationTest {
     ));
 
     orderService.order(new OrderRequest(
-        userKey,
+        this.getUserKey(),
         PaymentType.WALLET,
         "서울 강남구",
         LocalDateTime.now(),
@@ -96,7 +73,7 @@ public class OrderIntegrationTest {
     ));
 
     orderService.order(new OrderRequest(
-        userKey,
+        this.getUserKey(),
         PaymentType.WALLET,
         "서울 강남구",
         LocalDateTime.now(),
